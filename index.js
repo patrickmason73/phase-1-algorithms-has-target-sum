53f2574f662dc0fdf76e5425db6c1673f18f72f8
Patrick Mason <patrickmason73@gmail.com>
2023-01-02 22:49:21 -0700

    commit tree 3868d7d39b8ca2b9e49824badbcdbe9a215ddd42

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  
+  for(let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[i] + array[j] === target) 
+        return true
+      
+  
 }
+}
+return false
+}
+
 
 /* 
   Write the Big O time complexity of your function here
@@ -8,10 +19,15 @@ function hasTargetSum(array, target) {
 
 /* 
   Add your pseudocode here
+  iterate through array
+  if sum of two numbers equals target,
+  return true
 */
 
 /*
   Add written explanation of your solution here
+  take each number and add it to each of 
+  the other numbers to check if any add up to the target number
 */
 
 // You can run `node index.js` to view these console logs
